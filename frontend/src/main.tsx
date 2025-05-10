220a586c98006cc58bfece2040e0ff90c9c31108
MrGeorge88 <mrgeorge.enriquez@gmail.com>
2025-05-09 19:39:32 -0500

    Fix Tailwind CSS import and landing page text

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './tailwind.css'
 import './global-reset.css'
 import './components/ui/modal.css'
 import App from './App.tsx'
